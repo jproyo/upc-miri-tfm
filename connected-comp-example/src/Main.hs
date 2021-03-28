ee8f20e42b5dc5641a5c166a598a841b86202d98
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-03-28 16:53:46 +0200

    Testing streamly

--- connected-comp-example/src/Main.hs
+++ connected-comp-example/src/Main.hs
@@ -1,5 +1,25 @@
 module Main where
 
+import Streamly
+import Streamly.Prelude as S
+import Streamly.Internal.Prelude as SI
+import Relude as R
+import Control.Concurrent
+import System.IO (hSetBuffering, BufferMode(LineBuffering))
+
+data Prog = Continue | Eof
+  deriving (Show, Eq)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = hSetBuffering stdout LineBuffering >> prog
+    
+prog :: IO ()
+prog = 
+    S.drainWhile (/= Eof)
+     $ S.mapM (\(x,s) -> myThreadId >>= putTextLn . mappend ("Output Value: " <> show x <> " - Signal: " <> show s <> " - ThreadId: ") . show >> return s)
+    |$ S.mapM (\(x,s) -> myThreadId >>= putTextLn . mappend ("Filter " <> show x <> " Value: " <> show x <> " - Signal: " <> show s <> " - ThreadId: ") . show >> return (if x == 5 || s == Eof then (x,Eof) else (x,Continue)))
+    |$ S.mapM (\(x::Integer) -> myThreadId >>= putTextLn . mappend ("Input Value: " <> show x <> " - ThreadId: ") . show >> return (if x == 5 then (x,Eof) else (x+1,Continue)))
+    |$ S.enumerateFromTo 1 100
+  
+
+
