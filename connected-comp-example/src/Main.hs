22dfef00d41ea662b4e353cc1b15166282c5eda4
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-03-31 18:33:43 +0200

    Test streamly

--- connected-comp-example/src/Main.hs
+++ connected-comp-example/src/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import Streamly
-import Streamly.Internal.Prelude as SI
+import Streamly.Internal.Data.Stream.StreamK as SD
+--import Streamly.Internal.Prelude as SI
 import Streamly.Prelude as S
 import Relude as R
 import System.IO (hSetBuffering, BufferMode(LineBuffering))
@@ -20,7 +21,7 @@ prog = S.drain
      $ dynamicPipeline dataSource
 
 dynamicPipeline :: IsStream t => t IO Integer -> t IO ()
-dynamicPipeline s = s |& input |& generator |& output
+dynamicPipeline s = s |& input |& filterGenerator |& generator |& output
 
 dataSource :: IsStream s => s IO Integer
 dataSource = S.fromFoldable ([1..20]::[Integer]) 
@@ -28,14 +29,27 @@ dataSource = S.fromFoldable ([1..20]::[Integer])
 input :: IsStream s => s IO Integer -> s IO Integer
 input = fmap id
 
-generator :: IsStream s => s IO Integer -> s IO Integer 
-generator s = SI.foldrS (\a b -> if even a then b |& newFilter a else b) (generator' s) s
+filterGenerator :: IsStream s => s IO Integer -> s IO Integer 
+filterGenerator s = SD.foldlS (\b a -> if even a then b |& newFilter a else b) s s
 
 newFilter :: IsStream s => Integer -> s IO Integer -> s IO Integer
-newFilter y = S.mapM (\x -> myThreadId >>= (R.putStrLn . mappend (show x) . mappend (" - Filter " <> show y <> " - ") . show) >> return (if x == y then x+20 else x))
+newFilter y = S.mapM (filtering y)
 
-generator' :: IsStream s => s IO Integer -> s IO Integer
-generator' = S.mapM (\x -> myThreadId >>= (R.putStrLn . mappend (show x) . mappend " - " . show) >> return x)
+--filtering :: (Eq b, Num b, Show b) => b -> b -> IO b
+filtering :: (Eq b, Num b) => b -> b -> IO b
+filtering y x = do 
+  if x == 2 then threadDelay 1000000 else pure ()
+  -- tid <- myThreadId
+  -- R.putStrLn . mappend (show x) . mappend (" - Filter " <> show y <> " - ") . show $ tid
+  return (if x == y then x+20 else x)
+
+generator :: IsStream s => s IO Integer -> s IO Integer
+generator = S.mapM return 
+  -- where
+  --   generating x = do 
+  --     tid <- myThreadId 
+  --     R.putStrLn . mappend (show x) . mappend " - " . show $ tid
+  --     return x
 
 output :: IsStream s => s IO Integer -> s IO ()
 output = S.mapM print
