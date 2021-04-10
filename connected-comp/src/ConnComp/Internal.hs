b32980704e762b2a7661674ec9551b0442372bb8
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-04-10 17:55:33 +0200

    commit

--- connected-comp/src/ConnComp/Internal.hs
+++ connected-comp/src/ConnComp/Internal.hs
@@ -4,19 +4,34 @@ module ConnComp.Internal
   ) where
 
 --import           Control.Monad.Catch
+import Control.Concurrent
 import Control.Concurrent.Async
 import Data.ByteString as B
 import Control.Concurrent.STM.TChan as TC
---import           Data.ConnComp                                     as DC
+import           Data.ConnComp                                     as DC
 import           Relude                                            as R
-import GHC.IO.Handle
 
+
+type Stream = Maybe
 
 runParallelDP :: Handle -> IO ()
 runParallelDP h = do
   (input, a) <- fromInput h
-  b <- toOutput input
-  mapM_ wait [a,b]
+  (in', b) <- map' toEdge input
+  (out, as) <- generator in' 
+  final <- map' R.putStrLn out
+  mapM_ wait (a:b:final:as)
+
+generator :: TChan (Stream (Edge Integer)) -> IO (TChan (Stream (ConnectedComponents Integer)), [Async ()])
+generator = error "not implemented"
+
+map' :: (a -> IO b) -> TChan (Stream a) -> IO (Async ())
+map' f inCh = 
+  async $ loop inCh 
+  where 
+    loop ic = do 
+      e <- atomically (readTChan ic)
+      maybe (pure ()) (\a -> f a >> loop ic) e
 
 runDPConnectedComp :: IO ()
 runDPConnectedComp = do
@@ -24,29 +39,30 @@ runDPConnectedComp = do
   R.withFile file ReadMode runParallelDP
 
 
-toOutput :: TChan ByteString -> IO (Async ())
+toOutput :: TChan (Stream (ConnectedComponents Integer)) -> IO (Async ())
 toOutput inCh =
   async $ loop inCh 
   where 
     loop ic = do 
-      whenM (atomically $ isEmptyTChan ic) $ return ()
-      atomically (readTChan ic) >>= print
-      loop ic
+      e <- atomically (readTChan ic)
+      maybe (pure ()) (\a -> R.putStrLn (show a) >> loop ic) e
 
 
-fromInput :: Handle -> IO (TChan ByteString, Async ())
+fromInput :: Handle -> IO (TChan (Stream ByteString), Async ())
 fromInput h = do
   inputChannel <- newTChanIO
-  a <- async $ loop h inputChannel
+  a <- async $ loop inputChannel
   return (inputChannel, a)
  where
-  loop h' inCh = do
-    let bol = getAny <$> foldMapM (fmap Any) [hIsClosed h, R.hIsEOF h]
-    whenM bol $ return ()
-    whenM (not <$> bol) $ do
-      x <- hGetSome h' (1024 * 256)
-      atomically $ writeTChan inCh x
-      loop h' inCh
+  loop inCh = do
+    eof <- R.hIsEOF h
+    if eof
+        then atomically $ writeTChan inCh Nothing
+        else do
+            x <- B.hGetNonBlocking h (1024 * 256)
+            atomically $ writeTChan inCh (Just x)
+            loop inCh
+
         
 
 
