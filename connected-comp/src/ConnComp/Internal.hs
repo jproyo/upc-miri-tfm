e0dddc4304c085e66d1dafdf6c902a8412f22ace
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-04-11 07:38:35 +0200

    commit

--- connected-comp/src/ConnComp/Internal.hs
+++ connected-comp/src/ConnComp/Internal.hs
@@ -4,7 +4,6 @@ module ConnComp.Internal
   ) where
 
 --import           Control.Monad.Catch
-import Control.Concurrent
 import Control.Concurrent.Async
 import Data.ByteString as B
 import Control.Concurrent.STM.TChan as TC
@@ -17,36 +16,99 @@ type Stream = Maybe
 runParallelDP :: Handle -> IO ()
 runParallelDP h = do
   (input, a) <- fromInput h
-  (in', b) <- map' toEdge input
-  (out, as) <- generator in' 
-  final <- map' R.putStrLn out
+  (in', b) <- input |>> parseEdges
+  (out, as) <- generator in' =<< newTChanIO 
+  final <- mapM' (R.putStrLn . show) out
   mapM_ wait (a:b:final:as)
 
-generator :: TChan (Stream (Edge Integer)) -> IO (TChan (Stream (ConnectedComponents Integer)), [Async ()])
-generator = error "not implemented"
+generator :: TChan (Stream (Edge Integer)) -> TChan (Stream (ConnectedComponents Integer)) -> IO (TChan (Stream (ConnectedComponents Integer)), [Async ()])
+generator chn outChn = loop chn []
+  where
+    loop c xs = do 
+      e <- pull c
+      case e of 
+        Nothing -> end outChn >> return (outChn, xs)
+        Just v  -> do
+          newInput  <- newTChanIO 
+          a <- async $ newFilter (toConnectedComp v) c newInput outChn
+          loop newInput (a:xs)
 
-map' :: (a -> IO b) -> TChan (Stream a) -> IO (Async ())
-map' f inCh = 
-  async $ loop inCh 
+newFilter :: ConnectedComponents Integer -> TChan (Stream (Edge Integer)) -> TChan (Stream (Edge Integer)) -> TChan (Stream (ConnectedComponents Integer)) -> IO ()
+newFilter conn inCh toInCh outCh = do
+  e <- pull inCh
+  case e of 
+    Nothing -> conn >-> outCh >> return ()
+    Just v  ->
+      if v `includedIncident` conn 
+        then do 
+          let newList = v `addToConnectedComp` conn
+          newFilter newList inCh toInCh outCh
+        else do 
+          v >-> toInCh
+          newFilter conn inCh toInCh outCh
+
+dup :: TChan (Stream a) -> IO (TChan (Stream a))
+dup inCh = do 
+  newCh <- newTChanIO 
+  loop newCh
+  return newCh
   where 
-    loop ic = do 
-      e <- atomically (readTChan ic)
-      maybe (pure ()) (\a -> f a >> loop ic) e
+    loop newCh = do 
+      e <- pull inCh
+      maybe (end newCh) (\a -> a >-> newCh >> loop newCh) e 
 
+mapM' :: (a -> IO b) -> TChan (Stream a) -> IO (Async ())
+mapM' f inCh = 
+  async loop 
+  where 
+    loop = do 
+      e <- pull inCh
+      maybe (pure ()) (\a -> f a >> loop) e
+
+end :: TChan (Stream a) -> IO ()
+end c = atomically $ writeTChan c Nothing
+
+(>->) :: a -> TChan (Stream a) -> IO ()
+(>->) e c = atomically $ writeTChan c $ Just e
+
+pull :: TChan (Stream a) -> IO (Maybe a)
+pull c = atomically $ readTChan c
+
+(|>) :: TChan (Stream a) -> (a -> IO b) -> IO (TChan (Stream b), Async ())
+(|>) inp f = do 
+  newC <- newTChanIO
+  a <- async $ loop newC
+  return (newC, a)
+  where 
+    loop newCh = do 
+      e <- pull inp
+      maybe (end newCh) (\a -> loopE newCh a >> loop newCh) e
+
+    loopE newCh a = do
+      newVal <- f a
+      newVal >-> newCh
+
+(|>>) :: TChan (Stream a) -> (a -> IO [b]) -> IO (TChan (Stream b), Async ())
+(|>>) inp f = do 
+  newC' <- newTChanIO
+  a <- async $ loop newC'
+  return (newC', a)
+  where 
+    loop newCh = do 
+      e <- pull inp
+      maybe (end newCh) (\a -> loopE newCh a >> loop newCh) e
+
+    loopE newCh a = do
+      newVal <- f a
+      mapM_ (>-> newCh) newVal 
+      
 runDPConnectedComp :: IO ()
 runDPConnectedComp = do
   file <- maybe (liftIO $ fail "Error no parameter found") return . R.viaNonEmpty R.head =<< liftIO getArgs
   R.withFile file ReadMode runParallelDP
 
-
-toOutput :: TChan (Stream (ConnectedComponents Integer)) -> IO (Async ())
-toOutput inCh =
-  async $ loop inCh 
-  where 
-    loop ic = do 
-      e <- atomically (readTChan ic)
-      maybe (pure ()) (\a -> R.putStrLn (show a) >> loop ic) e
-
+parseEdges :: ByteString -> IO [Edge Integer]
+parseEdges = toEdge . decodeUtf8
 
 fromInput :: Handle -> IO (TChan (Stream ByteString), Async ())
 fromInput h = do
@@ -57,10 +119,10 @@ fromInput h = do
   loop inCh = do
     eof <- R.hIsEOF h
     if eof
-        then atomically $ writeTChan inCh Nothing
+        then end inCh
         else do
             x <- B.hGetNonBlocking h (1024 * 256)
-            atomically $ writeTChan inCh (Just x)
+            x >-> inCh
             loop inCh
 
         
