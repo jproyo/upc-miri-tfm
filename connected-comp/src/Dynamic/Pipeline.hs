4e0b01e4cc3b61859a8dc2f415c8b523812204ee
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-04-12 23:24:45 +0200

    unagi

--- connected-comp/src/Dynamic/Pipeline.hs
+++ connected-comp/src/Dynamic/Pipeline.hs
@@ -1,13 +1,14 @@
 module Dynamic.Pipeline where
 
 import           Control.Concurrent.Async
-import           Control.Concurrent.STM.TQueue                     as TC
+--import           Control.Concurrent.STM.TQueue                     as TC
+import Control.Concurrent.Chan.Unagi
 import           Relude                                                                                        hiding ( map
                                                                                                                       , mapM
                                                                                                                       , traverse
                                                                                                                       )
 
-type Channel a = TQueue (Maybe a)
+type Channel a = (InChan (Maybe a), OutChan (Maybe a))
 
 data Stream a = Stream
   { channel :: Channel a
@@ -24,30 +25,30 @@ end :: Stream a -> IO ()
 end = end' . channel
 
 end' :: Channel a -> IO ()
-end' = atomically . flip writeTQueue Nothing
+end' = flip writeChan Nothing . fst
 
 (>->) :: a -> Stream a -> IO ()
 (>->) e = (e -->) . channel
 
 (-->) :: a -> Channel a -> IO ()
-(-->) e s = atomically . writeTQueue s $ Just e
+(-->) e s = writeChan (fst s) (Just e)
 
 pull :: Stream a -> IO (Maybe a)
 pull = pull' . channel
 
 pull' :: Channel a -> IO (Maybe a)
-pull' = atomically . readTQueue
+pull' = readChan . snd
 
 (|>>) :: Foldable f => Stream a -> (a -> IO (f b)) -> IO (Stream b)
 (|>>) inp f = do
-  newC' <- newTQueueIO
+  newC' <- newChan
   Stream newC' <$> async (loop newC')
  where
   loop newCh = pull inp >>= loopUntilDone newCh (loopE newCh) loop
 
   loopE newCh a = do
     newVal <- f a
-    void $ atomically $ foldMapM (writeTQueue newCh . Just) newVal
+    void $ foldMapM (--> newCh) newVal
 
 loopUntilDone :: Channel b -> (a -> IO ()) -> (Channel b -> IO ()) -> Maybe a -> IO ()
 loopUntilDone ch f loop = maybe (end' ch) ((>> loop ch) . f)
@@ -55,7 +56,7 @@ loopUntilDone ch f loop = maybe (end' ch) ((>> loop ch) . f)
 -- Generate Stream base on a seed function `f`
 unfoldM :: IO b -> IO Bool -> IO (Stream b)
 unfoldM f stop = do
-  newCh <- newTQueueIO
+  newCh <- newChan
   Stream newCh <$> async (loop newCh)
   where loop newCh = ifM stop (end' newCh) (f >>= (--> newCh) >> loop newCh)
 
