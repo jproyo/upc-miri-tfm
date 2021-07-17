188f7bad55db3dac67a4f072b5e72149ffd34239
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-07-17 19:08:31 +0200

    First version

---
+++ bt-graph-dp/src/Edges.hs
@@ -0,0 +1,75 @@
+-- |
+-- Module      : Edges
+-- Copyright   : (c) 2021 Juan Pablo Royo Sales
+--
+-- License     : BSD3
+-- Maintainer  : juanpablo.royo@gmail.com
+-- Stability   : experimental
+-- Portability : GHC
+
+module Edges where
+
+import           Data.IntSet                                       as IS
+import           Data.Set                                          as S
+import           Relude                                            as R
+import           Text.Trifecta
+import           Text.Trifecta.Parser                              as P
+
+type LowerVertex = Int
+type UpperVertex = Int
+type Edge = (UpperVertex, LowerVertex)
+
+data W = W
+  { _wLowerVertex :: LowerVertex
+  , _wWedges      :: IntSet
+  }
+  deriving Show
+
+addWedge :: IntSet -> UpperVertex -> IntSet
+addWedge = flip IS.insert
+
+type UT = Set (UpperVertex, UpperVertex, UpperVertex)
+
+data DW = DW
+  { _dwLower :: (LowerVertex, LowerVertex)
+  , _dwUpper :: UT
+  }
+  deriving Show
+
+newtype DWTT = DWTT [DW]
+
+addDw :: DW -> DWTT -> DWTT
+addDw e (DWTT dw) = DWTT $ e : dw
+
+hasNotDW :: DWTT -> Bool
+hasNotDW (DWTT x) = R.null x
+
+data BT = BT
+  { _btLower :: (LowerVertex, LowerVertex, LowerVertex)
+  , _btUpper :: UT
+  }
+  deriving Show
+
+newtype BTTT = BTTT [BT]
+
+addBt :: BT -> BTTT -> BTTT
+addBt e (BTTT bt) = BTTT $ e : bt
+
+hasNotBT :: BTTT -> Bool
+hasNotBT (BTTT x) = R.null x
+
+nonEdge :: Edge
+nonEdge = (-1, -1)
+
+toEdge :: String -> Edge
+toEdge = foldResult (const nonEdge) identity . toEdge'
+
+toEdge' :: String -> Text.Trifecta.Result Edge
+toEdge' = P.parseString parseEdge mempty
+
+parseInt :: Parser Int
+parseInt = fromInteger <$> integer
+
+parseEdge :: Parser Edge
+parseEdge = (,) <$> (whiteSpace *> parseInt <* whiteSpace) <*> parseInt
+
--- bt-graph-dp/src/Main.hs
+++ bt-graph-dp/src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Relude
+
 main :: IO ()
 main = do
   putStrLn "hello world"
