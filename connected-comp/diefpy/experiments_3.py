bf8832455f1f55be06fdf1b65d28ea7b188aebb2
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2022-01-13 23:20:18 +0100

    moving forward

--- connected-comp/diefpy/experiments_3.py
+++ connected-comp/diefpy/experiments_3.py
@@ -3,20 +3,18 @@ import diefpy.dief as diefpy
 def plot_experiments():
   root_path = '/Users/juan/Projects/mios/upc-miri-tfm/connected-comp/results/diefpy/dp-dpfh'
   folders = ["ca-AstroPh", "email-Enron", "web-Google"]
-  #folders = ["ca-AstroPh"]
   colors = ["#ECC30B","#00CC00"]
   for f in folders:
     traces = diefpy.load_trace(f'{root_path}/{f}/results.csv')
     diefpy.plot_answer_trace(traces, f, colors).show()
-    # metrics = diefpy.load_metrics(f"{root_path}/{f}/metrics.csv")
-    # exp1 = diefpy.performance_of_approaches_with_dieft(traces, metrics)
-    # diefpy.plot_performance_of_approaches_with_dieft(exp1, f, colors).show()
+    metrics = diefpy.load_metrics(f"{root_path}/{f}/metrics.csv")
+    exp1 = diefpy.performance_of_approaches_with_dieft(traces, metrics)
+    diefpy.plot_performance_of_approaches_with_dieft(exp1, f, colors).show()
 
 
 def dief_t_k():
   root_path = '/Users/juan/Projects/mios/upc-miri-tfm/connected-comp/results/diefpy/dp-dpfh'
-  # folders = ["ca-AstroPh", "email-Enron", "web-Google"]
-  folders = ["ca-AstroPh"]
+  folders = ["ca-AstroPh", "email-Enron", "web-Google"]
   for f in folders:
     traces = diefpy.load_trace(f'{root_path}/{f}/results.csv')
     print("dief@t", f)
@@ -29,6 +27,6 @@ def dief_t_k():
     for (_, s, n) in diefpy.diefk(traces, f):
       print('&', s, '&', '$'+"{:.2E}".format(round(n,2))+'$', '\\\\')
 
-#dief_t_k()
-plot_experiments()
+dief_t_k()
+#plot_experiments()
 
